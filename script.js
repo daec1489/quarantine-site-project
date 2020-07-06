013a6038b7819e09d3e56ee77c81e7e3cd24e777
daec1489 <danacallejo89@gmail.com>
2020-07-06 13:57:54 +0800

    Removed live clock and added sidenav

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ window.addEventListener ("load", function() {
 setTimeout(function(){overlay.style.display = 'none';}, 2000);
 
 }); 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
   }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
   }
