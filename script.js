400a966ac46a170d77927f5b2f49466b694c4056
daec1489 <danacallejo89@gmail.com>
2020-07-06 16:51:08 +0800

    Added push content effect to sidenav bar and ease transition to sidenav toggle button

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ setTimeout(function(){overlay.style.display = 'none';}, 2000);
 
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0";
   }
