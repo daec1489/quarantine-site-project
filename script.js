b0439cbb43743b59eff588c5101b785e0dc2e780
daec1489 <danacallejo89@gmail.com>
2020-07-09 11:36:46 +0800

    Lessened nav button's default opacity

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var loader = document.getElementById('overlay');
+let loader = document.getElementById("overlay");
 
-window.addEventListener ("load", function() {
-setTimeout(function(){overlay.style.display = 'none';}, 2000);
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    overlay.style.display = "none";
+  }, 2000);
+});
 
-}); 
-
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0px";
+  document.getElementById("main").style.marginLeft = "0";
+}
