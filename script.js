c84c2a361ab42be1205bd3482c7d0e46bf68ad66
daec1489 <danacallejo89@gmail.com>
2020-07-14 10:56:58 +0800

    Changed 'important' selector to 'center'; Removed sidenav's push content effect

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ window.addEventListener("load", function () {
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0px";
-  document.getElementById("main").style.marginLeft = "0";
 }
